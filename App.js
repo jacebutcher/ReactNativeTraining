37f12e4aba592db968d370903e30ed9b7171000b
jdbutche <jace.dylan55@gmail.com>
2022-10-20 19:46:51 -0700

    Adding signup page and navigation

--- App.js
+++ App.js
@@ -1,105 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MainTabNavigator from './navigation/MainTabNavigator';
 
-export default class App extends React.Component {
-  state={
-    email:"",
-    password:""
-  }
-  
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text style={styles.logo}>Welcome!</Text>
-        <View style={styles.inputView} >
-          <TextInput  
-            style={styles.inputText}
-            placeholder="Email..." 
-            placeholderTextColor="white"
-            onChangeText={text => this.setState({email:text})}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Password..." 
-            placeholderTextColor="white"
-            onChangeText={text => this.setState({password:text})}/>
-        </View>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.create}>Or Create a New Account</Text>
-        </TouchableOpacity>
-        <View>
-        <Image resizeMode={'contain'} style={styles.image} source={require("./images/hydrip.png")} />
-        </View>
-        <Text style={styles.disclaimer}>Hydrip Wellness 2023©</Text>
+function App() {
+  return (
+    <View style={styles.container}>
+        <MainTabNavigator />
       </View>
-    );
-  }
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#72d9e9",
-    marginBottom:80
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#D4D4D4",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  forgot:{
-    color:"black",
-    fontSize:14
-  },
-  create:{
-    color:"black",
-    fontSize:14
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#72d9e9",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white",
-    fontSize: 20
-  },
-  image: {
-    width: 180,
-    height: 60,
-    marginTop: 80
-},
-  disclaimer:{
-    color:"black",
-    fontSize:10,
-    marginTop: 5
+    backgroundColor: '#fff',
   }
 });
+
+export default App;
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import LoginScreen from './screens/Login';
+import SignupScreen from './screens/Signup';
+
+const Tab = createBottomTabNavigator();
+
+function MainTabNavigator() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={'Login'}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            switch (route.name) {
+              case 'Login':
+                iconName = `home`;
+                break;
+              case 'Signup':
+                iconName = `flash`;
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          activeTintColor: 'blue',
+          inactiveTintColor: 'black',
+          style: { padding: 10, height: 70}
+        }}>
+        <Tab.Screen name={'Login'} component={LoginScreen} />
+        <Tab.Screen name={'Signup'} component={SignupScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainTabNavigator;
